088a8aac3f50368f8c7a4d7caf374178a7c33456
Gaurav Mishra <gauravmishras208013@gmail.com>
2023-04-20 05:19:48 +0530

    made some changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import {BrowserRouter} from "react-router-dom"
+import {About,Contact,Experience,Hero,Navbar,Feedbacks,Tech,Works,StarsCanvas} from './components'
+import Stars from './components/canvas/Stars'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <BrowserRouter>
+             <div className='relative z-0 bg-primary'>
+              <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
+                   <Navbar/>
+                   <Hero/>
+              </div>
+               <div className='mt-24 '>
+               <About/>
+               </div>
+               {/* <Experience/> */}
+               <Tech/>
+               <Works/>
+              
+              
+               <div className='relative z-0'>
+             
+               <Contact/>
+                <StarsCanvas/>
+               </div>
+             </div>
+  
+    </BrowserRouter>
   )
 }
 
