1cfe5c428939955d3df6c95badd96900f82eccf9
Gaurav Mishra <gauravmishras208013@gmail.com>
2023-04-21 01:21:24 +0530

    message

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,29 @@
-import React from 'react'
-import {BrowserRouter} from "react-router-dom"
-import {About,Contact,Experience,Hero,Navbar,Feedbacks,Tech,Works,StarsCanvas} from './components'
-import Stars from './components/canvas/Stars'
+import { BrowserRouter } from "react-router-dom";
+
+import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
 
 const App = () => {
   return (
     <BrowserRouter>
-             <div className='relative z-0 bg-primary'>
-              <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
-                   <Navbar/>
-                   <Hero/>
-              </div>
-               <div className=''>
-               <About/>
-               </div>
-               {/* <Experience/> */}
-               <Tech/>
-               <Works/>
-              
-              
-               <div className='relative z-0'>
-             
-               <Contact/>
-                <StarsCanvas/>
-               </div>
-             </div>
-  
+      <div className='relative z-0 bg-primary'>
+        <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
+          <Navbar />
+          
+        </div>
+       
+    
+       
+        <div className='relative z-0'>
+        <Hero />
+        <About />
+        <Tech />
+        <Works />
+          <Contact />
+          <StarsCanvas />
+        </div>
+      </div>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,47 +1,49 @@
-import React from 'react'
-import { motion } from 'framer-motion'
-import { styles } from '../style'
-import { ComputersCanvas } from './canvas'
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { ComputersCanvas } from "./canvas";
 
 const Hero = () => {
   return (
-    <section className='relative w-full h-screen mx-auto'>
-       <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
-          <div className='flex flex-col justify-center items-center mt-5'>
-            <div className='w-5 h-5 rounded-full bg-[#915eff]'/>
-            <div className='w-1 sm:h-80 h-40 violet-gradient'/>
-            </div>
-            <div>
-            <h1 className={`${styles.heroHeadText} text-white`}>Hi, I'm  <span className='text-[#915eff]'>Gaurav</span></h1>
-            <p className={`${styles.heroSubText} mt-2 text-white-100 text-[10px]`}>I am a frontend web developer <br className='sm:block hidden'/>who is passionate about creating visually appealing and interactive user interfaces that deliver seamless user experiences.</p>
-            
-            </div>
+    <section className={`relative w-full h-screen mx-auto flex flex-col`}>
+      <div
+        className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
+      >
+        <div className='flex flex-col justify-center items-center mt-5'>
+          <div className='w-5 h-5 rounded-full bg-[#915EFF]' />
+          <div className='w-1 sm:h-80 h-40 violet-gradient' />
+        </div>
 
-       </div>
-       
-       <ComputersCanvas/>
-       <div className=' -translate-y-16 xs:bottom-10 w-full flex justify-center items-center top-10'>
-        <a href="#about">
-          <div className='w-[28px]  h-[50px] rounded-3xl border-4 border-secondary flex justify-center items-start p-1'>
+        <div>
+          <h1 className={`${styles.heroHeadText} text-white`}>
+            Hi, I'm <span className='text-[#915EFF]'>Gaurav</span>
+          </h1>
+          <p className={`${styles.heroSubText} mt-2 text-white-100 text-[10px]`}>I am a frontend web developer <br className='sm:block hidden'/>who is passionate about creating visually appealing and interactive user interfaces that deliver seamless user experiences.<br className='sm:block hidden'/>I have a strong background in web design, user interface design, and user experience design, and use that knowledge to create visually appealing and intuitive front-end experiences. My skills also include responsive design and performance optimization.</p>
+          
+        </div>
+      </div>
+
+      {/* <ComputersCanvas /> */}
+
+      <div className='absolute -translate-y-5 xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
+        <a href='#about'>
+          <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
             <motion.div
               animate={{
-                y:[0,24,0]
+                y: [0, 24, 0],
               }}
-              transition={
-                {
-                  duration:1.5,
-                  repeat:Infinity,
-                  repeatType:'loop'
-                }
-                
-              }
-              className="w-3 h-3 rounded-full bg-secondary mb-1 "
+              transition={{
+                duration: 1.5,
+                repeat: Infinity,
+                repeatType: "loop",
+              }}
+              className='w-3 h-3 rounded-full bg-secondary mb-1'
             />
           </div>
         </a>
-       </div>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
